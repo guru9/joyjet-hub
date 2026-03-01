ee3621108071e907eabc7ca1d5ba5f888f684b7e
Gururaj Moger <gururaj9m@gmail.com>
2026-03-01 16:02:03 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
+  // api.cache(true) tells Babel to cache the config for faster builds.
+  // Use api.cache(false) during troubleshooting if changes aren't reflecting.
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env"
-      }]
-    ]
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+      // If you are using Reanimated, it must ALWAYS be the last plugin in this list.
+      // 'react-native-reanimated/plugin',
+    ],
   };
 };
